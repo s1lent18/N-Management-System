35555e5e68090e3621e7618aed31b16deb2657c1
s1lent18 <76154840+s1lent18@users.noreply.github.com>
2023-03-19 20:42:06 +0500

    Add files via upload

---
+++ Project.cpp
@@ -0,0 +1,170 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <ctime>
+using namespace std;
+
+string exacttime()
+{
+    string Time, temp;
+
+    time_t c_time = time(0);
+
+    char * TIME = ctime(&c_time);
+
+    Time = Time + TIME[8];
+
+    Time = Time + TIME[9];
+
+    temp = temp + TIME[4];
+
+    temp = temp + TIME[5];
+
+    temp = temp + TIME[6];
+
+    Time = Time + " ";
+
+    if (temp == "Jan")
+    {
+        Time = Time + "01";
+
+    }else if (temp == "Feb")
+    {
+        Time = Time + "02";
+
+    }else if (temp == "Feb")
+    {
+        Time = Time + "02";
+        
+    }else if (temp == "Mar")
+    {
+        Time = Time + "03";
+        
+    }else if (temp == "Apr")
+    {
+        Time = Time + "05";
+        
+    }else if (temp == "Jun")
+    {
+        Time = Time + "06";
+        
+    }else if (temp == "Jul")
+    {
+        Time = Time + "07";
+        
+    }else if (temp == "Aug")
+    {
+        Time = Time + "08";
+        
+    }else if (temp == "Sep")
+    {
+        Time = Time + "09";
+        
+    }else if (temp == "Oct")
+    {
+        Time = Time + "10";
+        
+    }else if (temp == "Nov")
+    {
+        Time = Time + "11";
+        
+    }else if (temp == "Dec")
+    {
+        Time = Time + "12";
+        
+    }
+
+    Time = Time + " ";
+
+    Time = Time + TIME[20];
+
+    Time = Time + TIME[21];
+
+    Time = Time + TIME[22];
+
+    Time = Time + TIME[23];
+
+    return Time;
+
+    
+
+}
+
+class Hospital
+{
+    private:
+        string name;
+        string fathername;
+        string mothername;
+        string birthdate;
+        string birthtime;
+        string deathtime;
+        string deathdate;
+        char gender;
+
+    public:
+        Hospital()
+        {
+            birthdate = exacttime();
+        }
+        void setname(string name)
+        {
+            this->name = name;
+        }
+
+        void setfathername(string fathername)
+        {
+            this->fathername = fathername;
+        }    
+
+        void setmothername(string mothername)
+        {
+            this->mothername = mothername;
+        }
+
+        void setgender(char gender)
+        {
+            this->gender = gender;
+        }
+
+        string getbirthdate()
+        {
+            return birthdate;
+        }
+
+
+};
+
+class Nadra
+{
+    private:
+        string name;
+        string fathername;
+        string mothername;
+        string f_grandfather;
+        string f_grandmother;
+        string m_grandfather;
+        string m_grandmother;
+        string CNIC;
+        string birthdate;
+        string birthtime;
+        string deathdate;
+        string deathtime;
+        string religion;
+        string relationshipstatus;
+        char gender;     
+        int marriagepaper; 
+        int age;
+
+    public:
+        string today_date;
+
+
+};
+
+int main()
+{
+    Hospital H;
+
+    cout<<H.getbirthdate();
+}
